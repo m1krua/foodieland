2745abc83bb073306543a820bb05805b8b73b359
malika <anarbaevamalika555@gmail.com>
2025-12-01 21:23:17 +0600

    home-page done

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -75,7 +75,7 @@ const cards = [
         like: "../images/graylike.svg"
     },
     {
-        image: "../images/beef.png",
+        image: "../images/loop2.png",
         name: "Big and Juicy Wagyu Beef Cheeseburger",
         text: "Western",
         like: "../images/graylike.svg"
@@ -132,5 +132,114 @@ for (const card of cards) {
         likeImg.src = likeImg.src.includes("redlike.svg")
             ? "../images/graylike.svg"
             : "../images/redlike.svg";
+    });
+}
+
+const posts = [
+    "../images/in1.png",
+    "../images/in2.png",
+    "../images/in3.png",
+    "../images/in4.png"
+]
+
+const instagram = document.querySelector(".instagram-cards")
+
+for (const post of posts) {
+    instagram.innerHTML += ` <img src="${post}" alt="">`
+}
+
+
+
+const loops = [
+    {
+        image: "../images/loop1.png",
+        title: "Mixed Tropical Fruit Salad with Superfood Boosts",
+        healthy: "Healthy",
+        like: "../images/redlike.svg"
+    },
+    {
+        image: "../images/loop2.png",
+        title: "Big and Juicy Wagyu Beef Cheeseburger",
+        healthy: "Western",
+        like: "../images/graylike.svg"
+    },
+    {
+        image: "../images/loop3.png",
+        title: "Healthy Japanese Fried Rice with Asparagus",
+        healthy: "Healthy",
+        like: "../images/redlike.svg"
+    },
+    {
+        image: "../images/loop4.png",
+        title: "Cauliflower Walnut Vegetarian Taco Meat",
+        healthy: "Eastern",
+        like: "../images/graylike.svg"
+    },
+    {
+        image: "../images/loop5.png",
+        title: "Rainbow Chicken Salad with Almond Honey Mustard Dressing",
+        healthy: "Healthy",
+        like: "../images/redlike.svg"
+    },
+    {
+        image: "../images/loop6.png",
+        title: "Barbeque Spicy Sandwiches with Chips ",
+        healthy: "Snack",
+        like: "../images/graylike.svg"
+    },
+    {
+        image: "../images/loop7.png",
+        title: "Firecracker Vegan Lettuce Wraps - Spicy! ",
+        healthy: "Seafood",
+        like: "../images/graylike.svg"
+    },
+    {
+        image: "../images/loop8.png",
+        title: "Chicken Ramen Soup with Mushroom ",
+        healthy: "Japanese",
+        like: "../images/redlike.svg"
+    }
+]
+
+
+
+
+const loopsBottom = document.querySelector(".loop-cards");
+loopsBottom.innerHTML = "";
+
+for (const loop of loops) {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("l-card");
+
+    cardDiv.innerHTML = `
+        <div class="l-like">
+            <img class="like-btn" src="${loop.like}" alt="like">
+        </div>
+
+        <div class="loop-top">
+            <img src="${loop.image}" alt="">
+        </div>
+
+        <div class="card-bottom">
+            <h3>${loop.title}</h3>
+            <div class="small-btn">
+                <span><img src="../images/time.svg" alt="">30 Minutes</span>
+                <span><img src="../images/spun.svg" alt="">${loop.healthy}</span>
+            </div>
+        </div>`
+        ;
+
+    loopsBottom.appendChild(cardDiv);
+
+    const likeImg = cardDiv.querySelector(".like-btn");
+
+    likeImg.addEventListener("click", (e) => {
+        e.stopPropagation();
+
+        if (likeImg.src.includes("redlike.svg")) {
+            likeImg.src = "../images/graylike.svg";
+        } else {
+            likeImg.src = "../images/redlike.svg";
+        }
     });
 }
